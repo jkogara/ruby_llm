0a90575104f7bd33d99db6f544a99a51a3acd372
John O'Gara <johnogara@gmail.com>
2025-10-22 15:01:32 +0100

    Adds an image module for vertexai

---
+++ lib/ruby_llm/providers/vertexai/images.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+module RubyLLM
+  module Providers
+    class VertexAI
+      # Image generation methods for the Gemini API implementation
+      module Images
+        def images_url
+          "projects/#{@config.vertexai_project_id}/locations/#{@config.vertexai_location}" \
+            "/publishers/google/models/#{@model}:predict"
+        end
+
+        def render_image_payload(prompt, model:, size:)
+          RubyLLM.logger.debug "Ignoring size #{size}. Gemini does not support image size customization."
+          @model = model
+          {
+            instances: [
+              {
+                prompt: prompt
+              }
+            ],
+            parameters: {
+              sampleCount: 1
+            }
+          }
+        end
+
+        def parse_image_response(response, model:)
+          data = response.body
+          image_data = data['predictions']&.first
+
+          unless image_data&.key?('bytesBase64Encoded')
+            raise Error, 'Unexpected response format from Gemini image generation API'
+          end
+
+          mime_type = image_data['mimeType'] || 'image/png'
+          base64_data = image_data['bytesBase64Encoded']
+
+          Image.new(
+            data: base64_data,
+            mime_type: mime_type,
+            model_id: model
+          )
+        end
+      end
+    end
+  end
+end
